51b4bdcf1ce5267c1beac399549d5a078865fe7f
Alberto Nieto <94092485+KingMuerte@users.noreply.github.com>
2023-07-05 14:50:34 +0000

    Format create_synthetic_data.py

--- src/create_synthetic_data.py
+++ src/create_synthetic_data.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from faker import Faker
 
 # Initialize a Faker object
@@ -9,7 +9,16 @@ fake = Faker()
 np.random.seed(123)
 
 # List of specific words
-specific_words = ["groceries", "rent", "refund", "reimbursement", "dining", "shopping", "credit", "debit"]
+specific_words = [
+    "groceries",
+    "rent",
+    "refund",
+    "reimbursement",
+    "dining",
+    "shopping",
+    "credit",
+    "debit",
+]
 
 # Generate random transaction descriptions
 transaction_descriptions = []
@@ -17,23 +26,23 @@ for _ in range(100):
     sentence = fake.sentence(nb_words=5).split()
     if np.random.rand() < 0.5:  # 50% chance to inject a specific word
         position = np.random.randint(len(sentence))  # choose a random position
-        sentence[position] = np.random.choice(specific_words)  # replace a word at this position
-    transaction_descriptions.append(' '.join(sentence))
+        sentence[position] = np.random.choice(
+            specific_words
+        )  # replace a word at this position
+    transaction_descriptions.append(" ".join(sentence))
 
 # Remove the period at the end of each description
-transaction_descriptions = [desc.rstrip('.') for desc in transaction_descriptions]
+transaction_descriptions = [desc.rstrip(".") for desc in transaction_descriptions]
 
 # Generate random transaction amounts
 transaction_amounts = np.random.choice(
-    [45, 50, 23.45, 78.12, 56.78, 12.34, 89.56, 34.12],
-    size=100
+    [45, 50, 23.45, 78.12, 56.78, 12.34, 89.56, 34.12], size=100
 )
 
 # Combine descriptions and amounts into a DataFrame
-transactions = pd.DataFrame({
-    "Description": transaction_descriptions,
-    "Amount": transaction_amounts
-})
+transactions = pd.DataFrame(
+    {"Description": transaction_descriptions, "Amount": transaction_amounts}
+)
 
 # Output the DataFrame to a CSV file
-transactions.to_csv('./data/transactions.csv', index=False)
+transactions.to_csv("./data/transactions.csv", index=False)
